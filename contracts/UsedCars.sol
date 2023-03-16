68d652848f8446b4ce109706c936db2c54233145
Alfredo Bravo Cuero <yo@alfredobravocuero.co>
2023-03-16 23:14:28 +0100

    basic project structuring for the management of used cars

---
+++ contracts/UsedCars.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract UsedCars {
+    uint public price;
+    address payable public seller;
+    address payable public buyer;
+
+    address[] previousBuyers;
+
+    enum State {
+        Sale,
+        Locked,
+        Release,
+        Closed,
+        Complete
+    }
+
+    State public state;
+
+    modifier condition(bool _condition) {
+        require(_condition);
+        _;
+    }
+
+    modifier onlyBuyer() {
+        require(msg.sender == buyer, "Only buyer can call this.");
+        _;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this.");
+        _;
+    }
+
+    modifier notSeller() {
+        require(msg.sender != seller, "Seller shouldn't call this.");
+        _;
+    }
+
+    modifier inState(State _state) {
+        require(state == _state, "Invalid state.");
+        _;
+    }
+
+    event Closed(uint256 when);
+    event ConfirmPurchase(uint256 when, address by);
+    event ConfirmReceived(uint256 when, address by);
+    event SellerRefundBuyer(uint256 when);
+    event SellerRefunded(uint256 when);
+    event Restarted(uint256 when);
+    event End(uint256 when);
+
+    constructor() payable {
+        require((2 * price) == msg.value, "El valor tiene que ser uniforme.");
+        seller = payable(msg.sender);
+        price = msg.value / 2;
+    }
+
+    function close() public onlySeller inState(State.Sale) {
+        state = State.Closed;
+        seller.transfer(address(this).balance);
+
+        emit Closed(block.timestamp);
+    }
+
+    function confirmPurchase()
+        public
+        payable
+        notSeller
+        inState(State.Sale)
+        condition(msg.value == (2 * price))
+    {
+        buyer = payable(msg.sender);
+        state = State.Locked;
+
+        emit ConfirmPurchase(block.timestamp, buyer);
+    }
+
+    function confirmReceived() public onlyBuyer inState(State.Locked) {
+        state = State.Release;
+
+        buyer.transfer(price); // El comprador recibe 1 x valor aquí
+        emit ConfirmReceived(block.timestamp, buyer);
+    }
+
+    function refundBuyer() public onlySeller inState(State.Locked) {
+        // Da la opción al vendedor de reembolsar al comprador antes de enviar un producto (automóvil) aquí
+        state = State.Sale;
+        buyer = payable(0);
+
+        emit SellerRefundBuyer(block.timestamp);
+    }
+
+    function refundSeller() public onlySeller inState(State.Release) {
+        state = State.Complete;
+        seller.transfer(3 * price);
+        previousBuyers.push(buyer);
+
+        emit SellerRefunded(block.timestamp);
+    }
+
+    function restartContract()
+        public
+        payable
+        onlySeller
+    {
+        if (state == State.Closed || state == State.Complete) {
+            require(
+                (2 * price) == msg.value,
+                "Value has to be equal to what started the contract."
+            );
+
+            state = State.Sale;
+
+            // Reset buyer to allow the same buyer again.
+            buyer = payable(0);
+            // This doesn't work.
+            // buyer = address(0);
+
+            emit Restarted(block.timestamp);
+        }
+    }
+
+    function listPreviousBuyers() public view returns (address[] memory) {
+        return previousBuyers;
+    }
+
+    // totalPreviousBuyers
+    function totalSales() public view returns (uint count) {
+        return previousBuyers.length;
+    }
+
+    function end() public onlySeller {
+        if (state == State.Closed || state == State.Complete) {
+            emit End(block.timestamp);
+
+            selfdestruct(seller);
+        }
+    }
+}
